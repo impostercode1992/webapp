c60a92b7ad3ed6fcdbdf1f0f520c79986b2f4999
impostercode1992 <mdshariefsalleh@gmail.com>
2025-10-14 23:58:58 +0800

    asd

--- oauth-service/g2auth.php
+++ oauth-service/g2auth.php
@@ -1,89 +1,122 @@
 <?php
-// add Google app config params
-include 'config/gconfig.php';
+session_start();
+$_SESSION['site'] = $_GET['site'];
+$_SESSION['returnPage'] = $_GET['page'];
+?>
 
-// ####################################################
-// Step 2: When Google redirects the user back here, 
-// there will be a "code" and "state"
-// parameter in the query string
-// Exchange the auth code for a JWT token,
-// and extract requested user data from there
-// ####################################################
-if(isset($_GET['code'])) {
-  
-  // Verify the state matches our stored state
-  if(!$_GET['state'] && $_GET['state'] !== '123') {    
-    die('missing state!');
-  }
-  
-function exchangeCodeForJWToken($code, $tokenURL, $googleClientID, $googleClientSecret, $redirectURL) {
-  // Exchange the auth code for a token
-  $ch = curl_init($tokenURL);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
-    'grant_type' => 'authorization_code',
-    'client_id' => $googleClientID,
-    'client_secret' => $googleClientSecret,
-    'redirect_uri' => $redirectURL,
-    'code' => $code
-  ]));  
+<html>
+    <head>
+        <style>
+            body {
+            font-size: 12px;
+            font-family: verdana;
+            }            
+
+            header {
+                text-align: center;
+                font-weight: bold;
+            }
+
+            header > div {
+                height: 50px;
+            }
+
+            #signInPanel {
+                display: flex;
+                flex-direction: column;
+                margin: 0px auto 24px;
+                width: 400px;
+                padding: 32px 40px;
+                background: rgb(255, 255, 255);
+                border-radius: 3px;
+                box-shadow: rgb(0 0 0 / 10%) 0px 0px 10px;
+                box-sizing: border-box;
+                color: rgb(94, 108, 132);
+            }
+
+            #apple-auth-button, #google-auth-button {
+                webkit-box-align: baseline;
+                align-items: baseline;
+                border-width: 0px;
+                border-radius: 3px;
+                box-sizing: border-box;
+                display: inline-flex;
+                font-size: inherit;
+                font-style: normal;
+                font-family: inherit;
+                max-width: 100%;
+                position: relative;
+                text-align: center;
+                text-decoration: none;
+                transition: background 0.1s ease-out 0s, box-shadow 0.15s cubic-bezier(0.47, 0.03, 0.49, 1.38) 0s;
+                white-space: nowrap;
+                cursor: pointer;
+                padding: 0px 10px;
+                vertical-align: middle;
+                width: 100%;
+                -webkit-box-pack: center;
+                justify-content: center;
+                font-weight: bold;
+                color: var(--ds-text,#42526E) !important;
+                height: 40px !important;
+                line-height: 40px !important;
+                background: rgb(255, 255, 255) !important;
+                box-shadow:  rgb(0 0 0 / 20%) 1px 1px 5px 0px !important           
+            }
+
+            #apple-auth-button > img, #google-auth-button > img {
+                width:20px;
+                height: 20px;
+                position: relative;
+                top: 3px;
+                padding-right: 20px;                
+            }
+        </style>
+
+    </head>
+    <body>
+        <header>
+            <div></div>
+            SIGN IN SERVICE
+            <div></div>
+        </header>
+        <div id="signInPanel">
+            <!-- Sign-In with Apple -->
+            <div class="signInAppleWrapper">
+                <form action="https://appleid.apple.com/auth/authorize?" method="get">
+                    <input type="hidden" id="client_id" name="client_id" value="com.toni-develops.public.oauth-with-all-providers-service" />
+                    <input type="hidden" id="redirect_uri" name="redirect_uri" value="https://public.toni-develops.com/examples/oauth/oauth-service/callbacks/callback.php?provider=apple" />
+                    <input type="hidden" id="response_type" name="response_type" value="code id_token"/>
+                    <input type="hidden" id="scope" name="scope" value="name email" />
+                    <input type="hidden" id="response_mode" name="response_mode" value="form_post" />
+
+                    <button id="apple-auth-button" type="submit" tabindex="0">
+                        <img class="appleLogo" src="https://www.toni-develops.com/external-files/examples/assets/apple-logo.svg" alt="">
+                        <span>Continue with Apple</span>
+                    </button>
+                </form>
+            </div>
 
 
-  $response = curl_exec($ch);
+            <!-- Sign-In with Google -->
+                        <div class="signInGoogleWrapper">
+                <form action="https://accounts.google.com/o/oauth2/v2/auth" method="get">
+                    <input type="hidden" id="client_id" name="client_id" value="53470530043-5gvhfeap2r8e2vjdjbjl82q22u75pq5k.apps.googleusercontent.com" />
+                    <input type="hidden" id="redirect_uri" name="redirect_uri" value="https://www.norsats.my/oauth-service/callbacks/callback.php?provider=google" />
+                    <input type="hidden" id="response_type" name="response_type" value="code"/>
+                    <input type="hidden" id="scope" name="scope" value="openid email profile" />
+                    <input type="hidden" id="state" name="state" value="123" />
 
-  // $data will contain 
-  // access_token, 
-  // expires_in, 
-  // scope, 
-  // token_type, 
-  // id_token - which is JWT token, containing user data according to requested scope from the initial script.
-  $data = json_decode($response, true);
-  //echo '<pre>';print_r($data);die("</pre>");
-
-  // Note: You'd probably want to use a real JWT library
-  // but this will do in a pinch. This is only safe to do
-  // because the ID token came from the https connection
-  // from Google rather than an untrusted browser redirect
-
-  // Split the JWT string into three parts
-  $jwt = explode('.', $data['id_token']);
-
-  // Extract the middle part, base64 decode it, then json_decode it
-  $IDToken = json_decode(base64_decode($jwt[1]), true);
-  
-  // This step is required only if we want to get extra info
-  /*
-  $ch = curl_init($usserInfoURL);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    'Authorization: Bearer '.$data['access_token']
-  ]);
-  $userInfo = curl_exec($ch);
-  */
-  /*
-  echo '<h1>Token ID</h1>';
-  echo '<pre>';print_r($IDToken);echo '</pre>';
-  echo '<h1>User info</h1>';
-  echo '<pre>';print_r($userInfo);die('</pre>');
-  */
-
-$userData = exchangeCodeForJWToken($_GET['code'], $tokenURL, $googleClientID, $googleClientSecret, $redirectURL);
+                    <button id="google-auth-button" class="css-11s2kpt" type="submit" tabindex="0">
+                        <img src="https://www.toni-develops.com/external-files/examples/assets/google-logo.svg" alt="Sign-in with Google">
+                        <span>Continue with Google</span>
+                    </button>
+                </form>
+            </div>
+        </div>
 
 
-// prepare parameters to pass back to the main app
-$params = array(
-  'email'       => $userData['email'],
-  'name'        => $userData['name'],
-  'given_name'  => $userData['given_name'],
-  'family_name' => $userData['family_name'],
-  'returnPage'  => $returnPage
-);
 
-$returnUrl = $baseUrl . $returnPage;
 
-if($authMethod === "redirect") {
-  // Redirect the user to the initial app passing user data as Query String parameters so the front end could use them.
-  header('Location: ' . $returnUrl . '?' . http_build_query($params));
-} else if($authMethod === "popupRedirect") {
-  include '../sign-in-popup.php';
-}
+    </body>
+</html>
--- script.js
+++ script.js
@@ -20,4 +20,3 @@ function getCookie(name) {
 function eraseCookie(name) {   
   document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
-
