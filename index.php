5c1684eb8fc13b27875f5464ef9461d4a436edd8
impostercode1992 <mdshariefsalleh@gmail.com>
2025-10-15 00:02:51 +0800

    asd

--- index.php
+++ index.php
@@ -22,18 +22,10 @@
     <body onload="initOnLoad()">
         <div id="myAccount">
                         <div class="signInGoogleWrapper">
-                <form action="https://accounts.google.com/o/oauth2/v2/auth" method="get">
-                    <input type="hidden" id="client_id" name="client_id" value="53470530043-5gvhfeap2r8e2vjdjbjl82q22u75pq5k.apps.googleusercontent.com" />
-                    <input type="hidden" id="redirect_uri" name="redirect_uri" value="https://www.norsats.my/oauth-service/callbacks/callback.php?provider=google" />
-                    <input type="hidden" id="response_type" name="response_type" value="code"/>
-                    <input type="hidden" id="scope" name="scope" value="openid email profile" />
-                    <input type="hidden" id="state" name="state" value="123" />
-
-                    <button id="google-auth-button" class="css-11s2kpt" type="submit" tabindex="0">
-                        <img src="https://www.toni-develops.com/external-files/examples/assets/google-logo.svg" alt="Sign-in with Google">
+                          <a href="https://www.norsats.my/oauth-service/sign-in.php?site=sitetwo&page=index.php">kkk</a>
+"
                         <span>Continue with Google</span>
-                    </button>
-                </form>
+
             </div>
         </div>     
 <?php // include("wc/navbar.html");?>
